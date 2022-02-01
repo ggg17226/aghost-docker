4296d7ec821809ce3e5258f7de5edd714fe08814
aghost <ggg17226@gmail.com>
2022-02-01 14:06:46 +0800

    feat: update jdk 11 and add jdk 17

---
+++ jdk-11/build-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker buildx inspect --bootstrap
+docker buildx prune -a -f && docker builder prune -a -f
+docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
+--tag agh0st/jdk:11.0.13p8-`date "+%Y%m%d-%H%M-%Z"` \
+--push .
---
+++ jdk-17/build-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker buildx inspect --bootstrap
+docker buildx prune -a -f && docker builder prune -a -f
+docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
+--tag agh0st/jdk:17.0.1p12-`date "+%Y%m%d-%H%M-%Z"` \
+--tag agh0st/jdk:latest \
+--push .
