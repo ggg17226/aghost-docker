66ebaa2424efffa79be5e94896a7873ad98862e9
aghost <ggg17226@gmail.com>
2022-04-01 15:56:35 +0800

    feat: add php 7.4

---
+++ php-7/build-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker buildx inspect --bootstrap
+docker buildx prune -a -f && docker builder prune -a -f
+docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
+--tag agh0st/php:7.4.3-`date "+%Y%m%d-%H%M-%Z"` \
+--tag agh0st/php:latest \
+--push .
