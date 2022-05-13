3b52be33a74ff6d3eda431dfee89e4cedf0f3690
aghost <ggg17226@gmail.com>
2022-05-13 15:10:58 +0800

    feat: update ubuntu bionic and focal add ubuntu jammy migrate ubuntu latest to jammy

--- ubuntu-jammy/build-image.sh
+++ ubuntu-jammy/build-image.sh
@@ -2,5 +2,6 @@
 docker buildx inspect --bootstrap
 docker buildx prune -a -f && docker builder prune -a -f
 docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
---tag agh0st/ubuntu:focal-`date "+%Y%m%d-%H%M-%Z"` \
+--tag agh0st/ubuntu:latest \
+--tag agh0st/ubuntu:jammy-`date "+%Y%m%d-%H%M-%Z"` \
 --push .
