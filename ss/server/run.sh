494072fbfc0144331216e6f0b037280ca741560d
aghost <ggg17226@gmail.com>
2022-06-14 11:24:57 +0800

    fix: command error

--- ss/server/run.sh
+++ ss/server/run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 cd /app
+ulimit -n 6553500
 ./ss-server \
--s "${SS_SERVER_HOST:-0.0.0.0}" \
--p ${SS_SERVER_PORT:-14444} \
--m "${SS_SERVER_METHOD:-xchacha20-ietf-poly1305}" \
--k "${SS_SERVER_PASSWORD:-$HOSTNAME}" \
--t ${SS_SERVER_TIMEOUT:-600} \
--n ${SS_SERVER_OPEN_FILE_LIMIT:-32767} \
---no-delay ${SS_SERVER_DNS_ARGS} \
+--server-addr "${SS_SERVER_HOST:-[::]}:${SS_SERVER_PORT:-14444}" \
+--encrypt-method "${SS_SERVER_METHOD:-xchacha20-ietf-poly1305}" \
+--password "${SS_SERVER_PASSWORD:-$HOSTNAME}" \
+--timeout ${SS_SERVER_TIMEOUT:-600} \
+--nofile ${SS_SERVER_OPEN_FILE_LIMIT:-32767} \
+--tcp-no-delay \
+${SS_SERVER_DNS_ARGS} \
 ${SS_SERVER_EXT_ARGS}
