3251a79c59e3f0f748aefcef7b67ee750cb5c360
aghost <ggg17226@gmail.com>
2022-06-14 11:42:46 +0800

    fix: command error

--- ss/server/run.sh
+++ ss/server/run.sh
@@ -4,8 +4,10 @@ cd /app
 --server-addr "${SS_SERVER_HOST:-[::]}:${SS_SERVER_PORT:-14444}" \
 --encrypt-method "${SS_SERVER_METHOD:-xchacha20-ietf-poly1305}" \
 --password "${SS_SERVER_PASSWORD:-$HOSTNAME}" \
---timeout ${SS_SERVER_TIMEOUT:-600} \
+--timeout ${SS_SERVER_TIMEOUT:-60} \
 --nofile ${SS_SERVER_OPEN_FILE_LIMIT:-32767} \
---tcp-no-delay \
+--tcp-no-delay --tcp-fast-open \
+--tcp-keep-alive ${SS_SERVER_KEEPALIVE_TIMEOUT:-180} \
+${SS_SERVER_MODE:--U} \
 ${SS_SERVER_DNS_ARGS} \
 ${SS_SERVER_EXT_ARGS}
