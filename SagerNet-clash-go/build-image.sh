b4222d0732cdf82608914af745bf45c779dee7fe
aghost <ggg17226@gmail.com>
2022-07-16 18:00:58 +0800

    fix: build script error

--- SagerNet-clash-go/build-image.sh
+++ SagerNet-clash-go/build-image.sh
@@ -2,6 +2,6 @@
 docker buildx inspect --bootstrap
 docker buildx prune -a -f && docker builder prune -a -f
 docker buildx build --platform linux/amd64 --no-cache \
-  --tag agh0st/SagerNet-clash-go:latest \
-  --tag agh0st/SagerNet-clash-go:$(date "+%Y%m%d-%H%M-%Z") \
+  --tag agh0st/sagernet-clash-go:latest \
+  --tag agh0st/sagernet-clash-go:$(date "+%Y%m%d-%H%M-%Z") \
   --push .
