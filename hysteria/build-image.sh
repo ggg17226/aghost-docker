0f293c4a6c00af97d0eeba1588a285c496bcb772
aghost <ggg17226@gmail.com>
2023-01-06 12:36:17 +0800

    feat: update hy
    Signed-off-by: aghost <ggg17226@gmail.com>

--- hysteria/build-image.sh
+++ hysteria/build-image.sh
@@ -3,5 +3,5 @@ docker buildx inspect --bootstrap
 docker buildx prune -a -f && docker builder prune -a -f
 docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
   --tag agh0st/hysteria:latest \
-  --tag agh0st/hysteria:1.0.5-3762b07-$(date "+%Y%m%d-%H%M-%Z") \
+  --tag agh0st/hysteria:$(date "+%Y%m%d-%H%M-%Z") \
   --push .
