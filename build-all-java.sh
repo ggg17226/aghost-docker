44a1740ce79f1ce88f65ef320abc23a15d250b06
aghost <ggg17226@gmail.com>
2023-03-17 13:47:30 +0800

    feat: update java
    Signed-off-by: aghost <ggg17226@gmail.com>

--- build-all-java.sh
+++ build-all-java.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
-for i in `ll | grep -E 'jdk|jre' | awk '{print $9}'`;
-do echo ==========================$i; cd $i ; bash build-image.sh || break; done
+local_dir=`pwd`
+for i in `ls -laF $local_dir | grep -E 'jdk|jre' | awk '{print $9}'`;
+do \
+  working_dir=${local_dir}/${i}
+  echo ==========================$working_dir;\
+  cd $working_dir && \
+  bash ./build-image.sh || break\
+ done
