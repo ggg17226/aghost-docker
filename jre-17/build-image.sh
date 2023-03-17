ab17ab511c31ada01154f133f253d65be5543160
aghost <ggg17226@gmail.com>
2023-03-17 13:31:35 +0800

    feat: update java
    Signed-off-by: aghost <ggg17226@gmail.com>

---
+++ jre-17/build-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker buildx inspect --bootstrap
+docker buildx prune -a -f && docker builder prune -a -f
+docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
+--tag agh0st/jre:17-`date "+%Y%m%d-%H%M-%Z"` \
+--tag agh0st/jdk:latest \
+--push .
