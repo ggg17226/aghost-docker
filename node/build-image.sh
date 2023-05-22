50b397725ea459e1cae122837e6fa490774d07d2
aghost <ggg17226@gmail.com>
2023-05-22 18:02:46 +0800

    feat: update java
    Signed-off-by: aghost <ggg17226@gmail.com>

--- node/build-image.sh
+++ node/build-image.sh
@@ -3,5 +3,5 @@ docker buildx inspect --bootstrap
 docker buildx prune -a -f && docker builder prune -a -f
 docker buildx build --platform linux/arm64,linux/amd64 --no-cache \
 --tag agh0st/node:latest \
---tag agh0st/node:16.18.0-buster-`date "+%Y%m%d-%H%M-%Z"` \
+--tag agh0st/node:16.20.0-bullseye-`date "+%Y%m%d-%H%M-%Z"` \
 --push .
